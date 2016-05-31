6c4d984109361fd578cd1569dac452f4bd360b7f
Jessica Gineris <jessicag@support.com>
2016-05-31 15:47:46 -0700

    Add users.json and deviceinfo api

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var fs = require("fs");
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+var router = express.Router();
+
+
+router.get('/deviceinfo/:useremail', function(req, res) {
+    var email = req.params.useremail;
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+        data = JSON.parse( data );
+        console.log( 'data[email]', data[email]);
+        res.json(data[email]);
+    });
 });
 
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!');
+router.get('/hello', function(req, res) {
+    res.json({ message: 'Hello world' });
 });
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('App listening on port ' + port);
