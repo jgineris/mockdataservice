c2ddba2211cb80c27dcc3a8b354a611a211155fb
Jessica Gineris <jessicag@support.com>
2016-06-06 17:26:26 -0700

    Return a json with variable devices

--- controllers/device.js
+++ controllers/device.js
@@ -6,10 +6,9 @@ module.exports.getDeviceInfo = function(req, res) {
     console.log('email is', email);
     fs.readFile( __dirname + "/../" + "devices.json", 'utf8', function (err, data) {
         if(err) return res.status(400).send("Could not read devices");
-        console.log('error',err,'data', data);
         data = JSON.parse( data );
-        var user = data[email];
-        if(user) return res.json(user);
+        var devices = data[email];
+        if(devices) return res.json({devices: devices} );
         return res.status(400).send('User could not be found');
     });
 };
